0b37fa3aea9afd8b526290e48974642dd910d004
jmrapp1 <jmrapp1270@gmail.com>
2018-05-04 09:48:30 -0400

    added client-side error checking using mappers before dispatching

--- src/client/src/redux/modules/user/actions.ts
+++ src/client/src/redux/modules/user/actions.ts
@@ -2,27 +2,36 @@ import { dispatchRequest } from '../../utils/fetchUtils';
 import UserRegisterResource from '../../../../../shared/resources/user/UserRegisterResource';
 import * as Reducer from './reducers';
 import UserLoginResource from '../../../../../shared/resources/user/UserLoginResource';
+import UserRegisterMapper from '../../../../../shared/mappers/user/UserRegisterMapper';
+import { BadRequestError } from '../../../../../shared/errors/BadRequestError';
+import UserLoginMapper from '../../../../../shared/mappers/user/UserLoginMapper';
 const jwtDecode = require('jwt-decode');
 
 export function register(registerResource: UserRegisterResource, successCallback, errorCallback) {
-    return dispatch =>
-        dispatchRequest('api/user/register', 'POST', JSON.parse(JSON.stringify(registerResource)), data => {
+    return dispatch => {
+        const localErrors = UserRegisterMapper.verifyAllConstraints(registerResource);
+        if (localErrors) return errorCallback(new BadRequestError(localErrors));
+        return dispatchRequest('api/user/register', 'POST', JSON.parse(JSON.stringify(registerResource)), data => {
             successCallback(data);
             Reducer.register(data);
         }, err => {
             errorCallback(err);
         });
+    }
 }
 
 export function login(loginResource: UserLoginResource, successCallback, errorCallback) {
-    return dispatch =>
-        dispatchRequest('api/user/login', 'POST', JSON.parse(JSON.stringify(loginResource)), data => {
+    return dispatch => {
+        const localErrors = UserLoginMapper.verifyAllConstraints(loginResource);
+        if (localErrors) return errorCallback(new BadRequestError(localErrors));
+        return dispatchRequest('api/user/login', 'POST', JSON.parse(JSON.stringify(loginResource)), data => {
             localStorage.setItem('id_token', data.jwtToken);
             decodeUserDataToStoreFromLocal(dispatch);
             successCallback(data);
         }, err => {
             errorCallback(err);
         });
+    }
 }
 
 export function decodeUserDataToStoreFromLocal(dispatch) {
